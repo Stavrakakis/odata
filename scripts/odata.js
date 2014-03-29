cfb31c70ad2e394ee0dec6f45e05859ec528ca66
nico_stavrakakis <nico.stavrakakis@gmail.com>
2014-03-29 17:23:14 -0400

    odata now working with datatable

--- scripts/odata.js
+++ scripts/odata.js
@@ -14,13 +14,43 @@ var svc = 'http://services.odata.org/V3/Northwind/Northwind.svc/';
         
         var db;
         
-        $data.initService(context).then(function (database) {
+        OData.read( 
+          "http://services.odata.org/Northwind/Northwind.svc/Customers", 
+          function (data) { 
+            var results = data.results;
+            var columns = Object.getOwnPropertyNames(results[0]);
+            var index = columns.indexOf("__metadata");
             
-            db = database;
-            var customers = db.Customers.forEach(function(d){
-                console.log(d);
+            if (index > -1) {
+                columns.splice(index, 1);
+            }
+            
+            
+            
+            var table = d3.select('#customers');
+            
+            var thead = table.append("thead");
+            var tbody = table.append("tbody");
+            
+            var headers = thead.append("tr").selectAll("th").data(columns).enter().append("th").text(function(d){return d;});
+            
+            var rows = tbody.selectAll('tr').data(results);
+        
+            rows.enter().append('tr');
+            
+            var cells = rows.selectAll("td")
+                .data(function(row) {
+                    return columns.map(function(column) {
+                        return {column: column, value: row[column]};
+                    });
+                })
+                .enter()
+                .append("td")
+                .html(function(d) {return d.value; });
                 
-            });
-        });
+                $('#customers').dataTable();
+          } 
+        );
+        
     
     });
