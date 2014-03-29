cc0f66b0fc2d36c195fcc78f7b213b1c1ef3076c
Nico <nico@scrapbook.(none)>
2014-03-29 22:29:46 +0000

    added employee list

--- scripts/odata.js
+++ scripts/odata.js
@@ -1,73 +1,84 @@
-var svc = 'http://services.odata.org/V3/Northwind/Northwind.svc/';
-    var drawTable = function(columns, data, element){
-        var table = d3.select(element);
-            
-            var thead = table.append("thead");
-            var tbody = table.append("tbody");
-            
-            var headers = thead.append("tr").selectAll("th").data(columns).enter().append("th").text(function(d){return d;});
-            
-            var rows = tbody.selectAll('tr').data(data);
+﻿var svc = 'http://services.odata.org/V3/Northwind/Northwind.svc/';
+var drawTable = function(columns, data, element){
+    var table = d3.select(element);
         
-            rows.enter().append('tr');
-            
-            var cells = rows.selectAll("td")
-                .data(function(row) {
-                    return columns.map(function(column) {
-                        return {column: column, value: row[column]};
-                    });
-                })
-                .enter()
-                .append("td")
-                .html(function(d) {return d.value; });
-                
-                $(element).dataTable();
-    };
+        var thead = table.append("thead");
+        var tbody = table.append("tbody");
+        
+        var headers = thead.append("tr").selectAll("th").data(columns).enter().append("th").text(function(d){return d;});
+        
+        var rows = tbody.selectAll('tr').data(data);
     
-    var getPropertyNames = function(dataset){
-        var columns = Object.getOwnPropertyNames(dataset[0]);
-        var index = columns.indexOf("__metadata");
+        rows.enter().append('tr');
         
-        if (index > -1) {
-            columns.splice(index, 1);
-        }
-        return columns;
+        var cells = rows.selectAll("td")
+            .data(function(row) {
+                return columns.map(function(column) {
+                    return {column: column, value: row[column]};
+                });
+            })
+            .enter()
+            .append("td")
+            .html(function(d) {return d.value; });
+            
+            $(element).dataTable();
+};
+
+var getPropertyNames = function(dataset){
+    var columns = Object.getOwnPropertyNames(dataset[0]);
+    var index = columns.indexOf("__metadata");
+    
+    if (index > -1) {
+        columns.splice(index, 1);
+    }
+    return columns;
+};
+
+$(document).ready(function(){
+    
+    OData.defaultHttpClient.enableJsonpCallback = true;
+    
+    var context = {name: "oData",
+        url: svc,
+        oDataServiceHost: svc,
+        enableJsonpCallback: true,
+        maxDataServiceVersion: "3.0",
+        dataServiceVersion: "3.0" 
     };
-    $(document).ready(function(){
+            
+    OData.read( 
+      "http://services.odata.org/Northwind/Northwind.svc/Customers", 
+      function (data) { 
+        var results = data.results;
         
-        OData.defaultHttpClient.enableJsonpCallback = true;
+        var columns = getPropertyNames(results);
         
-        var context = {name: "oData",
-            url: svc,
-            oDataServiceHost: svc,
-            enableJsonpCallback: true,
-            maxDataServiceVersion: "3.0",
-            dataServiceVersion: "3.0" 
-        };
+        drawTable(columns, results, '#customers');            
         
-        var db;
+      } 
+    );
+    
+    OData.read( 
+      "http://services.odata.org/Northwind/Northwind.svc/Suppliers", 
+      function (data) { 
+        var results = data.results;
         
-        OData.read( 
-          "http://services.odata.org/Northwind/Northwind.svc/Customers", 
-          function (data) { 
-            var results = data.results;
-            
-            var columns = getPropertyNames(results);
-            
-            drawTable(columns, results, '#customers');            
-            
-          } 
-        );
+        var columns = getPropertyNames(results);
         
-        OData.read( 
-          "http://services.odata.org/Northwind/Northwind.svc/Products", 
-          function (data) { 
-            var results = data.results;
-            
-            var columns = getPropertyNames(results);
-            
-            drawTable(columns, results, '#products');            
-            
-          } 
-        );
-    });
+        drawTable(columns, results, '#suppliers');            
+        
+      } 
+    );
+    
+    OData.read( 
+      "http://services.odata.org/Northwind/Northwind.svc/Products", 
+      function (data) { 
+        var results = data.results;
+        
+        var columns = getPropertyNames(results);
+        
+        drawTable(columns, results, '#products');            
+        
+      } 
+    );
+});
