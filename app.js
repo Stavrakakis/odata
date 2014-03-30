af6da1ec2a3f88662a8c769f2d1432c98148a519
Nico <nico@scrapbook.(none)>
2014-03-30 09:21:14 +0000

    tagged initial version, included gruntfile

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 var express = require('express');
 var app = express();
 
-process.on('uncaughtException', function(err) {
-    if(err.errno === 'EADDRINUSE'){
-         console.log(err);
-         }
-    else{
-         console.log(err);
-         }
-    process.exit(1);
-});
-
-app.configure(function(){
-  app.use("/scripts", express.static(__dirname + '/scripts'));
+app.configure(function() {
+    app.use("/scripts", express.static(__dirname + '/scripts'));
 
 });
 
- app.get("/", function(req, res) {
-    res.sendfile(__dirname + '/index.html')
- });
+app.get("/", function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
 
 app.listen(4000);
-
---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                },
+            },
+            files: {
+                src: ['*.js', 'scripts/*.js']
+            }
+        },
+        "jsbeautifier": {
+            files: ["*.js", 'scripts/*.js'],
+            options: {
+                html: {
+                    braceStyle: "collapse",
+                    indentChar: " ",
+                    indentScripts: "keep",
+                    indentSize: 4,
+                    maxPreserveNewlines: 10,
+                    preserveNewlines: true,
+                    unformatted: ["a", "sub", "sup", "b", "i", "u"],
+                    wrapLineLength: 0
+                },
+                css: {
+                    indentChar: " ",
+                    indentSize: 4
+                },
+                js: {
+                    braceStyle: "collapse",
+                    breakChainedMethods: false,
+                    e4x: false,
+                    evalCode: false,
+                    indentChar: " ",
+                    indentLevel: 0,
+                    indentSize: 4,
+                    indentWithTabs: false,
+                    jslintHappy: false,
+                    keepArrayIndentation: false,
+                    keepFunctionIndentation: false,
+                    maxPreserveNewlines: 10,
+                    preserveNewlines: true,
+                    spaceBeforeConditional: true,
+                    spaceInParen: false,
+                    unescapeStrings: false,
+                    wrapLineLength: 0
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'jsbeautifier']);
+
+};
